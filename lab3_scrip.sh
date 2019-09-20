5d3bb6aba170915cbfa488ee63823fc2bfce2771
artem690 <49416764+artem690@users.noreply.github.com>
2019-09-20 12:48:43 -0600

    Update lab3_scrip.sh

--- lab3_scrip.sh
+++ lab3_scrip.sh
@@ -7,5 +7,10 @@ echo "Please input filename: "
 read fileName
 echo "Please input expression: "
 read expression
-
 grep $expression $fileName
+#counting number of phone numbers
+grep -c -P '[0-9]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt
+#counting number of numbers with 303
+grep -o -P '303-[0-9]{3}-[0-9]{4}' regex_practice.txt
+#number of emails 
+grep -o -P '[a-zA-Z0-9]*@geocities.com' regex_practice.txt
